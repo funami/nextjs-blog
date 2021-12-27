b3d720e09f44c3e69bb8af09f5fbf77699a82fa1
Takao Funami <takao.funami@gmail.com>
2021-12-27 17:19:45 +0900

    チュートリアルコミット

---
+++ components/profile.js
@@ -0,0 +1,14 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Profile() {
+  const { data, error } = useSWR(
+    "https://61c91de420ac1c0017ed8b47.mockapi.io/api/v1/user/1",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return <div>hello {data.name}!</div>;
+}
